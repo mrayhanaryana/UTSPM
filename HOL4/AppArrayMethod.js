a45ad7da3439f3bd8f51b7ea02c7e59a4dcfc4cc
ChanifRusydi <chanif.rusydi@gmail.com>
2021-11-07 21:37:50 +0700

    changes

--- HOL4/AppArrayMethod.js
+++ HOL4/AppArrayMethod.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Component } from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 class AppArrayMethod extends Component {
     render() {
       const buah = ['apple', 'banana', 'orange'];
     
     return (
+      <SafeAreaView>
         <View>
            <Text>Contoh Penggunaan Array Method</Text>
                  { buah.map((item) => (
            <Text key={item}>{item}</Text>
-        ))}
+        )
+        )}
         </View>
+      </SafeAreaView>
    );
   }
 }
