549c861b5ceed30abd62a639176b9752469f0a5c
mrayhanaryana <mrayhanaryana99@gmail.com>
2021-11-08 05:02:26 +0700

    Add

--- HOL2/App_customcompnama.js
+++ HOL2/App_customcompnama.js
@@ -4,7 +4,7 @@ import { Text, View, Image } from 'react-native';
 const AppHeader = () => {
 return (
     <View style={{height:60, backgroundColor: 'gray'}}>
-        <Text style={{fontWeight: 'bold',fontSize: 32}}>Aura</Text>
+        <Text style={{fontWeight: 'bold',fontSize: 32, textAlign: 'center'}}>M Rayhan A</Text>
     </View>
 );
 }
@@ -12,8 +12,8 @@ return (
 const AppContent = () => {
 return (
     <View>
-        <Text style={{fontWeight: 'bold',fontSize: 23}}>1103194173</Text>
-        <Image source={require('./Image/flutter.png')} style={{width: 300, height:50, resizeMode:'contain'}} />
+        <Text style={{fontWeight: 'bold',fontSize: 23, textAlign: 'center'}}>1103194042</Text>
+        <Image source={require('./Image/Rayhan.png')} style={{width: 400, height:450, resizeMode:'contain'}} />
     </View>
 );
 }
